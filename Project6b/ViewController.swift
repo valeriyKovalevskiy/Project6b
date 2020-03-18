2215ab430fec6bd2e3329923360539017c44414e
Valeriy Kovalevskiy <v.kovalevskiy.ios@gmail.com>
2020-03-18 10:32:01 +0300

    challenge completed

--- Project6b/ViewController.swift
+++ Project6b/ViewController.swift
@@ -49,12 +49,33 @@ class ViewController: UIViewController {
         view.addSubview(label4)
         view.addSubview(label5)
         
-        let viewsDictionary = ["label1": label1, "label2": label2 , "label3": label3, "label4": label4, "label5": label5]
+//        let viewsDictionary = ["label1": label1, "label2": label2 , "label3": label3, "label4": label4, "label5": label5]
+//        let metrics = ["labelHeight" : 100 ]
+//        viewsDictionary.keys.forEach{ (label) in
+//            view.addConstraints(NSLayoutConstraint.constraints(withVisualFormat: "H:|[\(label)]|", options: [], metrics: nil, views: viewsDictionary))
+//        }
+//        view.addConstraints(NSLayoutConstraint.constraints(withVisualFormat: "V:|[label1(labelHeight@999)]-[label2(label1)]-[label3(label1)]-[label4(label1)]-[label5(label1)]->=10-|", options: [], metrics: metrics, views: viewsDictionary))
+
+        //H - Horisontally. V - Vertical. | - edges of screen, "-" - spacing (by default = 10), @999 - priority
+
         
-        viewsDictionary.keys.forEach{ (label) in
-            view.addConstraints(NSLayoutConstraint.constraints(withVisualFormat: "H:|[\(label)]|", options: [], metrics: nil, views: viewsDictionary))
+        var previous: UILabel?
+        
+        for label in [label1, label2, label3, label4, label5] {
+            label.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor, constant: 0).isActive = true
+            label.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor, constant: 0).isActive = true
+            label.heightAnchor.constraint(equalToConstant: (view.frame.height / 5 )  - 10 ).isActive = true
+
+            if let previous = previous {
+                label.topAnchor.constraint(equalTo: previous.bottomAnchor, constant: 10).isActive = true
+            } else {
+                label.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 0).isActive = true
+            }
+            
+            previous = label
         }
-        view.addConstraints(NSLayoutConstraint.constraints(withVisualFormat: "V:|[label1]-[label2]-[label3]-[label4]-[label5]", options: [], metrics: nil, views: viewsDictionary))
+        
+
     }
 
 
